ae46a20a1a52dcb7dbbe1ab514dc7a1ea62634d1
Miracle <54377927+miraclehollins08@users.noreply.github.com>
2020-08-10 17:50:41 -0800

    Add files via upload

---
+++ Apps.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Stylessheet from "./Stylesheets"
+import Header from "./Header"
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Stylessheet />
+        </div>
+    )
+}
+
+export default App
---
+++ StyleSheet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './myStyles.css'
+
+function Stylesheet() {
+    return (
+        <div>
+            <h3 className="navbar"> Hi!</h3>
+        </div>
+    )
+}
+
+export default Stylesheet
